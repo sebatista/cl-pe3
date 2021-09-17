384dd3da78baf072df652294a84c3023c04f69d8
Sebas <sebatista@yahoo.com>
2021-09-17 10:27:06 -0300

    Update __manifest__.py
    Se elimina un repositorio inexistente para la versión 13.0 de ADHOC.
    https://github.com/ingadhoc/odoo-legal ingadhoc-odoo-legal

--- pe3_default/__manifest__.py
+++ pe3_default/__manifest__.py
@@ -143,7 +143,6 @@
 		'https://github.com/ingadhoc/multi-store ingadhoc-multi-store',
 		'https://github.com/ingadhoc/odoo-argentina ingadhoc-odoo-argentina',
 		'https://github.com/ingadhoc/odoo-argentina-ce ingadhoc-odoo-argentina-ce',
-		'https://github.com/ingadhoc/odoo-legal ingadhoc-odoo-legal',
 		'https://github.com/ingadhoc/odoo-public-administration ingadhoc-odoo-public-administration',
 		'https://github.com/ingadhoc/partner ingadhoc-partner',
 		'https://github.com/ingadhoc/product ingadhoc-product',
